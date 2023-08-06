b117e943fcf5c06e96fc39e27cfd1dddc257aa1b
Adrián <adrian.gaitan@utp.edu.co>
2023-08-05 19:50:26 -0500

    inital structure for the server

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
-import router from "./app/routes/index.routes";
+import router from "./app/routes/index.routes.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(Express.json());
+app.use(express.json());
 
 // Routes
 app.use('/api', router);
+
 app.get('/', (req, res) => {
     res.send('API is running...');
     } 
 );
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
